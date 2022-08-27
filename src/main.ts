de93cb1d4ddea9e5a58ac7e422c1eb15c4549022
jchongs80 <57120191+jchongs80@users.noreply.github.com>
2022-08-27 01:37:33 -0500

    fix

--- src/main.ts
+++ src/main.ts
@@ -22,7 +22,7 @@ async function bootstrap() {
       whitelist: true, //Validate body if accept any other field in json
     }),
   );
-  await app.listen(process.env.PORT || 8080);
+  await app.listen(8080);
 
   //Debug
   logger.log(`Server rendering in ${await app.getUrl()}`);
